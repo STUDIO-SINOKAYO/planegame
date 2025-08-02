0ebc881e4ae95fd68a0f9c443e90026cd26581a1
Mita <rwiriya123@gmail.com>
2025-08-02 19:42:03 -0400

    obstacles

--- Scripts/Water.gd
+++ Scripts/Water.gd
@@ -2,9 +2,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("Entered water")
-	body.gravity_strength *= 10
-
-
-func _on_body_exited(body: Node2D) -> void:
-	print("Exited water")
-	body.gravity_strength = body.gravity_strength/10
+	body.current_speed = 0
