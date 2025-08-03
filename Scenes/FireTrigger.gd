7297538d65e302c8e33f546bffd41acf65aa3a88
Mita <rwiriya123@gmail.com>
2025-08-03 11:28:12 -0400

    triggers

--- Scenes/FireTrigger.gd
+++ Scenes/FireTrigger.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+@onready var l: AnimationPlayer = $"../../Lightning/AnimationPlayer"
 
 
 func _on_body_entered(body: Node2D) -> void:
 	animation_player.play("fire appear")
+	l.play("leave")
--- Scenes/ResetTrigger.gd
+++ Scenes/ResetTrigger.gd
@@ -1,14 +1,10 @@
 extends Area2D
 
-@onready var house: AnimationPlayer = $"../Obstacles/HouseSnow3/AnimationPlayer"
 @onready var fire: AnimationPlayer = $"../Obstacles/Fire2/AnimationPlayer"
-@onready var lightning: AnimationPlayer = $"../Obstacles/Lightning/AnimationPlayer"
 
 
 func _on_body_entered(body: Node2D) -> void:
 	var plane := body as PlayerPlane
 	if not plane:
 		return
-	house.play("STAY")
 	fire.play("RESET")
-	lightning.play("STAY")
