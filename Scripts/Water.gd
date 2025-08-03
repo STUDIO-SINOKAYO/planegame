cea5c3cecde03a5ba1ccdc5d51740862dc88f4f9
Mita <rwiriya123@gmail.com>
2025-08-03 13:12:45 -0400

    AA

--- Scripts/Water.gd
+++ Scripts/Water.gd
@@ -4,7 +4,6 @@ class_name WaterObstacle
 ## Water area that slows down the plane
 
 @export var water_speed: float = 50.0
-@onready var water_audio: AudioStreamPlayer = $"../Water"
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -13,8 +12,5 @@ func _on_body_entered(body: Node2D) -> void:
 	var plane := body as PlayerPlane
 	if not plane:
 		return
-	
-	if water_audio:
-		water_audio.play()
 	
 	plane.current_speed = water_speed
